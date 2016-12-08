1cabf82f2d2b00baae1d96a5e6f756ebf9b7cc25
龐校長 <fspwz@foxmail.com>
2016-12-08 14:34:05 +0800

    加入setInterval，保证必定能执行对应代码

--- app/capture-example/dist/js/mmd_sfq.js
+++ app/capture-example/dist/js/mmd_sfq.js
@@ -4,49 +4,66 @@ if (!window.mmd) {
 
 // 打开ocr
 mmd.mmd_openOCR = function (mmdCallback) {
-  // 一次只能启动一个，如何做到？
-  sfq.capture.selectOCR(null, function ocrCallback(error, data) {
-    if (error) {
-      console.log(error);
-      // 错误处理...
-      mmdCallback('001', error.message);
-      return;
-    } else if (data.imageUrl) {
-      mmdCallback('000', data.imageUrl);
-    } else {
-      // 如果erro和data都是null，则是用户取消了。
-      alert('用户取消!');
-      mmdCallback('002', '用户取消');
+  var timer = setInterval(function() {
+    if (window.WebViewBridge) {
+      clearInterval(timer);
+      // 一次只能启动一个，如何做到？
+      sfq.capture.selectOCR(null, function ocrCallback(error, data) {
+        if (error) {
+          console.log(error);
+          // 错误处理...
+          mmdCallback('001', error.message);
+          return;
+        } else if (data.imageUrl) {
+          mmdCallback('000', data.imageUrl);
+        } else {
+          // 如果erro和data都是null，则是用户取消了。
+          alert('用户取消!');
+          mmdCallback('002', '用户取消');
+        }
+      });
     }
-
-  });
+  }, 500);
 }
 
 mmd.mmd_applyFinish = function (result) {
-  alert('apply', window.WebViewBridge);
-  // result = {code: '000', result: '申请成功'}
-  try {
-    sfq.mmd.applyFinish(result);
-  } catch (e) {
-    alert(e.message);
-  }
-  alert('apply');
+  var timer = setInterval(function() {
+    if (window.WebViewBridge) {
+      clearInterval(timer);
+      alert('apply', window.WebViewBridge);
+      // result = {code: '000', result: '申请成功'}
+      try {
+        sfq.mmd.applyFinish(result);
+      } catch (e) {
+        alert(e.message);
+      }
+      alert('apply');
+    }
+  }, 500);
 }
 
 mmd.mmd_activationFinish = function (result) {
-  // alert(1);
-  alert('activate', window.WebViewBridge);
-  // result = {code: '000', result: '申请成功'}
-  sfq.mmd.activateFinish(result);
-  alert('activate');
+  var timer = setInterval(function() {
+    if (window.WebViewBridge) {
+      clearInterval(timer);
+      alert('activate', window.WebViewBridge);
+      // result = {code: '000', result: '申请成功'}
+      sfq.mmd.activateFinish(result);
+      alert('activate');
+    }
+  }, 500);
 }
 
 mmd.mmd_payFinish = function (result) {
-  // alert(1);
-  alert('pay', window.WebViewBridge);
-  // result = {code: '000', result: '申请成功'}
-  sfq.mmd.payFinish(result);
-  alert('pay');
+  var timer = setInterval(function() {
+    if (window.WebViewBridge) {
+      clearInterval(timer);
+      alert('pay', window.WebViewBridge);
+      // result = {code: '000', result: '申请成功'}
+      sfq.mmd.payFinish(result);
+      alert('pay');
+    }
+  }, 500);
 }
 
 mmd.mmd_useCreditAfterActivation = function () {
